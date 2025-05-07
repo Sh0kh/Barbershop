8294a7637db4949ccd238c967966c365f9b06785
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-05-07 09:13:26 +0500

    edit

--- src/Components/Home/components/HomeHero.jsx
+++ src/Components/Home/components/HomeHero.jsx
@@ -2,44 +2,45 @@ import { ChevronRight, Globe } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 import logo from '../../UI/Icons/photo_2025-05-02_21-23-30.jpg';
 import BarbersOne from './BarbersOne';
+import Foto from '../../UI/Icons/kov00774-scaled.jpg'
 
 export default function HomeHero() {
   const { t, i18n } = useTranslation();
-  
+
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
 
   return (
     <div className="bg-gray-50 text-black w-full mx-auto max-w-xl relative">
-  
+
       <div className="absolute top-4 right-4 z-30">
         <div className="flex items-center bg-black bg-opacity-70 text-white rounded-full shadow-lg overflow-hidden">
-          <button 
+          <button
             onClick={() => changeLanguage('uz')}
             className={`px-4 py-2 transition-all flex items-center ${i18n.language === 'uz' ? 'bg-white text-black' : 'hover:bg-opacity-80'}`}
           >
             <Globe size={18} className="mr-2" />
-            <span>O'zbek</span>
+            <span>Uz</span>
           </button>
-          
+
           <div className="h-6 w-px bg-gray-400"></div>
-          
-          <button 
+
+          <button
             onClick={() => changeLanguage('ru')}
             className={`px-4 py-2 transition-all flex items-center ${i18n.language === 'ru' ? 'bg-white text-black' : 'hover:bg-opacity-80'}`}
           >
             <Globe size={18} className="mr-2" />
-            <span>Русский</span>
+            <span>Ru</span>
           </button>
         </div>
       </div>
 
       {/* Qolgan kontent o'zgarishsiz qoladi */}
       <div className="relative w-full h-70 bg-black">
-        <img 
-          src="https://assets.alteg.io/booking_form_appearance_header/origin/2/2a/2ac2bcb7a129f97_20241028031212.png" 
-          alt="23:59 Barbershop staff" 
+        <img
+          src={Foto}
+          alt="23:59 Barbershop staff"
           className="w-full h-full object-cover"
         />
         <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"></div>
